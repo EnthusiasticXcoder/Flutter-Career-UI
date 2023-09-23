583170369f35e3fbac5733336b15fcc5d9d96ef6
Anshul Verma <anshulverma1405@gmail.com>
2023-09-24 02:52:35 +0530

    added constants

---
+++ lib/constants/fields_constant.dart
@@ -0,0 +1,11 @@
+const payloadField = 'payload';
+const tokenfield = 'token';
+const firstNameField = 'first_name';
+const lastNameField = 'last_name';
+const emailField = 'email';
+const userNameField = 'username';
+const passwordField = 'password';
+
+const tagField = 'tag';
+const contantField = 'content';
+const linkField = 'link';
---
+++ lib/constants/paths.dart
@@ -0,0 +1,8 @@
+
+const baseURL = '192.168.29.230:8000';
+const registerPath = 'user/register';
+const loginPath = 'user/login';
+const userPath = 'user';
+const currierlistPath = 'blog/menu';
+const bloglistPath = 'blog/list';
+const blogContantPath = 'blog/contant';
